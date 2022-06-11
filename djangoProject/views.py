f2f04bef69319b242a5a860796ed7770f76d3d39
wyyadd <wyyadd@gmail.com>
2022-06-11 21:37:52 +0800

    添加了两种搜索模型

--- djangoProject/views.py
+++ djangoProject/views.py
@@ -12,5 +12,8 @@ def search(request):
     request.encoding = "utf-8"
     query = request.GET['query']
     search_type = request.GET['type']
-    result = searchEngine.search(query, dic, search_type)
+    if len(query) == 0 or len(search_type) == 0:
+        result = []
+    else:
+        result = searchEngine.search(query, dic, search_type)
     return render(request, "index.html", {"poem_list": result})
